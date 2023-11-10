a51b3bbebe3b496dafb0885b1dbeb3440a6984d9
Enailly <129402483+Enailly@users.noreply.github.com>
2023-11-10 11:38:19 -0300

    Update planilha.py

--- planilha.py
+++ planilha.py
@@ -1,25 +1,21 @@
-import pandas as pd
+import openpyxl 
 
-# Dados
-dados = [
-    ["Computador 1", "8gb ram", "R$ 2500"],
-    ["Computador 2", "16gb ram", "R$ 5500"],
-    ["Computador 3", "32gb ram", "R$ 8500"]
-]
+book = openpyxl.Workbook()
 
-# Nome da Planilha
-nome_planilha = "Meus computadores"
+print(book.sheetnames)
 
-# Nome da página
-nome_pagina = "Computadores"
+book.creat_sheet("Computadores")
 
-# Nome das colunas
-nomes_colunas = ["Eletrônica", "Memória ram", "Preço"]
+computadores_page = book("Computadores")
+computadores_page.apeend(["Eletrônica", "Memória ram", "preço"])
 
-# Criar um DataFrame com os dados
-df = pd.DataFrame(dados, columns=nomes_colunas)
+computadores_page.append(["Computador 1", "8gb ram", "R$ 2500"])
+computadores_page.append(["Computador 2", "16gb ram", "R$ 5500"])
+computadores_page.append(["Computador 3", "32gb ram", "R$ 8500"])
 
-# Criar um arquivo Excel com a planilha
-with pd.ExcelWriter(nome_planilha + '.xlsx') as writer:
-    df.to_excel(writer, sheet_name=nome_pagina, index=False)
+#salva a pĺanilha 
+book.save("planilha de computadores.xlsx")
+
+
+
 
